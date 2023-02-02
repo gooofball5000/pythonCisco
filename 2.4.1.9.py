b785c690ebaed92d57b6122c2cc57234a93a2b57
Jack <59679085+gooofball5000@users.noreply.github.com>
2023-02-02 11:03:16 -0600

    fix km to miles conversion

--- 2.4.1.9.py
+++ 2.4.1.9.py
@@ -2,7 +2,7 @@
 miles = 7.38
 
 miles_to_kilometers = miles  * 1.609344
-kilometers_to_miles = 1
+kilometers_to_miles = kilometers * 0.6213712
 
 print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
 print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
