27a2ac5e5a4b7e22839f9676589f6eeaf7e77d85
Jack <59679085+gooofball5000@users.noreply.github.com>
2023-02-08 10:12:15 -0600

    Was wrong file

--- 2.5.1.2.py
+++ 2.5.1.2.py
@@ -1,22 +1,15 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
-Hours:  2
-Seconds in Hours:  7200
->>> 
-========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
-Hours:  2
-Traceback (most recent call last):
-  File "C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py", line 8, in <module>
-    print("Seconds in Hours: ", a * secondsInHour) # printing the number of seconds in a given number of hours
-NameError: name 'a' is not defined
->>> 
-========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
-Hours:  2
-Seconds in Hours:  7200
->>> 
-========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
-Hours:  2
-Seconds in Hours:  7200
-Goodbye
+# 2.5.1.2.py
+#this program computes the number of seconds in a given number of hours
+
+
+numHours = 2
+secondsInHour = 3600 # number of seconds in 1 hour
+
+print("Hours: ", numHours)
+print("Seconds in Hours: ", numHours * secondsInHour) # printing the number of seconds in a given number of hours
+
+#TODO here we should also print "Goodbye", but a programmer didn't have time to write any code
+print("Goodbye")
+
+
+#this is the end of the program that computes the number of seconds in 3 hours
