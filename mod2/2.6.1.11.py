2d69f4974e8b2e31378b1a21f224c6339ecde7eb
Jack <59679085+gooofball5000@users.noreply.github.com>
2023-02-08 10:50:16 -0600

    modified 2.6.1.11

--- mod2/2.6.1.11.py
+++ mod2/2.6.1.11.py
@@ -1,4 +1,18 @@
-hour = int(input("Starting time (hours): "))
-mins = int(input("Starting time (minutes): "))
-dura = int(input("Event duration (minutes): "))
+# hour = int(input("Starting time (hours): "))
+# mins = int(input("Starting time (minutes): "))
+# dura = int(input("Event duration (minutes): "))
 
+
+hour = 12
+mins = 17
+dura = 59
+
+mins += dura
+result = hour, ":", mins
+
+def endTime(time):
+    return result
+
+
+print(hour, ":")
+print(endTime)
