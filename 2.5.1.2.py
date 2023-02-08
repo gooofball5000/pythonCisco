f552b3856d1a0e5a720586c9aad11cec431a69dc
Jack <59679085+gooofball5000@users.noreply.github.com>
2023-02-08 10:05:36 -0600

    Add 2.5.1.2

---
+++ 2.5.1.2.py
@@ -0,0 +1,22 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
+Hours:  2
+Seconds in Hours:  7200
+>>> 
+========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
+Hours:  2
+Traceback (most recent call last):
+  File "C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py", line 8, in <module>
+    print("Seconds in Hours: ", a * secondsInHour) # printing the number of seconds in a given number of hours
+NameError: name 'a' is not defined
+>>> 
+========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
+Hours:  2
+Seconds in Hours:  7200
+>>> 
+========= RESTART: C:\Users\n113924\Desktop\Python\Module 2\2.5.1.2.py =========
+Hours:  2
+Seconds in Hours:  7200
+Goodbye
