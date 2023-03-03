335757a3fa0511fb836aefa10cd8aab8723602fc
Jack <59679085+gooofball5000@users.noreply.github.com>
2023-03-03 11:38:42 -0600

    added more

---
+++ mod3/3.4.1.13.py
@@ -0,0 +1,21 @@
+﻿beatles = []
+
+
+# step 1
+print("Step 1:", beatles)
+
+# step 2
+print("Step 2:", beatles)
+
+# step 3
+print("Step 3:", beatles)
+
+# step 4
+print("Step 4:", beatles)
+
+# step 5
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
