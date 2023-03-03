513a3c2f05d49c889ec2e3d8b57218a025a1c069
Jack <59679085+gooofball5000@users.noreply.github.com>
2023-03-03 11:25:41 -0600

    y

---
+++ mod3/3.4.1.2.py
@@ -0,0 +1,15 @@
+﻿numbers = [10, 5, 7, 2, 1]
+print("Original list content:", numbers)  # Printing original list content.
+
+numbers[0] = 111
+print("\nPrevious list content:", numbers)
+
+numbers[1] = numbers[4]
+print("Previous list content:", numbers)
+
+print("\nList's length", len(numbers))
+
+
+del numbers[1]
+print("New list's length:", len(numbers))
+print("\nNew list content:", numbers)
